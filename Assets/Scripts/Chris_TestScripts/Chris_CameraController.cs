23d6aded5823b95d7b8e68432190e52f3f6fe933
cjabs1997 <cjabs97@gmail.com>
2020-10-19 11:46:50 -0700

    Simple movement and character swapping (camera only)
    -Adds lateral movement
    -Adds jumping
    -Can swap camera perspectives

---
+++ Assets/Scripts/Chris_TestScripts/Chris_CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chris_CameraController : MonoBehaviour
+{
+    public Target target;
+    public float yOffset;
+    public List<Transform> targets;
+
+    public enum Target
+    {
+        Fire = 0,
+        Ice = 1
+    }
+
+    private void Update()
+    {
+        this.transform.position = new Vector3(targets[(int)target].position.x, targets[(int)target].position.y + yOffset, this.transform.position.z);
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            target = (Target) (((int)target + 1) % 2);
+        }
+    }
+}
---
+++ Assets/Scripts/Chris_TestScripts/Chris_CharacterController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chris_CharacterController : MonoBehaviour
+{
+    [Header("Stats")]
+    [SerializeField] private float moveSpeed = 0f;
+    [SerializeField] private float jumpForce = 0f;
+
+    [Header("Misc")]
+    [SerializeField] private bool selected = false;
+
+
+    private Rigidbody2D m_Rigidbody2D;
+
+    private void Awake()
+    {
+        m_Rigidbody2D = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            m_Rigidbody2D.AddForce(Vector2.up * jumpForce);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float direction = Input.GetAxisRaw("Horizontal");
+
+        float jerk = moveSpeed * direction;
+
+        jerk *= ((moveSpeed - m_Rigidbody2D.velocity.magnitude) / moveSpeed) + 100f;
+
+        m_Rigidbody2D.AddForce(new Vector2(jerk, 0));
+    }
+
+    public void SwapSelected()
+    {
+        selected = !selected;
+    }
+}
