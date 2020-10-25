409bade434791c81431ac8e86c7dd45d96b907a0
cjabs1997 <cjabs97@gmail.com>
2020-10-25 10:45:26 -0700

    Add basic fling pad
    -Flings the object on it in the given direction with the given force
    -Only works in inspector but could be easily added to whatever mechanism we want

---
+++ Assets/Scripts/Chris_TestScripts/Chris_FlingPad.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Super basic, just flings a character along the given vector with the given force.
+/// 
+/// Currently isn't implemented fully, need to utilize the inspector to trigger it. But the logic is there, aka we just call the fling function when/where we want and it will work.
+/// Also could add a slight animation if we want, could add some more character to it.
+/// </summary>
+public class Chris_FlingPad : MonoBehaviour
+{
+    [Header("Fling Pad Settings")]
+    [Tooltip("How much force will be applied to the object when the pad flings.")]
+    public float flingForce;
+    [Tooltip("Vector at which the force will be applied. Will be normalized before applying the force.")]
+    public Vector2 flingAngle;
+
+    [Header("Debug")]
+    [Tooltip("The object currently being targeted by the fling pad. Currently only flings one object at a time. Visible for debug purposes only.")]
+    [SerializeField]private Rigidbody2D targetRigidBody;
+
+
+
+
+    public void Fling()
+    {
+        if(targetRigidBody != null)
+        {
+            targetRigidBody.AddForce(flingAngle.normalized * flingForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        targetRigidBody = collision.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        targetRigidBody = null;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Doesn't draw the line at the best spot but if this is something we want to use I can move it over/make it look better.
+        Gizmos.DrawLine(this.transform.position, (this.transform.position + (Vector3)flingAngle*3));
+    }
+}
---
+++ Assets/Scripts/Chris_TestScripts/Editor/FlingPadEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Chris_FlingPad))]
+public class FlingPadEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Chris_FlingPad flingPad = (Chris_FlingPad)target;
+
+        if (GUILayout.Button("Fling") && Application.isPlaying)
+        {
+            flingPad.Fling();
+        }
+    }
+}
