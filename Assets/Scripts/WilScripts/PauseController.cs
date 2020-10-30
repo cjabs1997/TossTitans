925c879dea0d651a093b30a57cebb0c4b5fc7ed1
wfaigen24 <wjfaigen@outlook.com>
2020-10-30 13:12:39 -0400

    Tutorial Pickups
    Added pickups for a potential tutorial level that will pause the game and show information. Placeholders in currently, but it pauses the game, any key continues, and destroys after.

---
+++ Assets/Scripts/WilScripts/PauseController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    public static bool isPaused = false;
+    private GameObject whichPopUp;
+    public Canvas popUpCanvas;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If the scene is currently paused due to a pop up, any key will resume time and close the pop up
+        if (isPaused)
+        {
+            if (Input.anyKeyDown)
+            {
+                isPaused = false;
+                Time.timeScale = 1f;
+                whichPopUp.SetActive(false);
+            }
+        }
+    }
+
+    public void Pause(GameObject popUp)
+    {
+        //Uncomment pausing logic after testing the UI showing up
+        isPaused = true;
+        Time.timeScale = 0f;
+
+        //Set the PopUp to set false equal to whichever gameobject was given by the pop up message
+        whichPopUp = popUp;
+
+        //Set its status to active. This set false above by any key
+        whichPopUp.SetActive(true);
+    }
+}
