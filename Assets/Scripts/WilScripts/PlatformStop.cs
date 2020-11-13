d0874feb1ce90c27eb47907c80c576babd63948c
wfaigen24 <wjfaigen@outlook.com>
2020-11-12 20:55:06 -0500

    1st level nearly done

---
+++ Assets/Scripts/WilScripts/PlatformStop.cs
@@ -0,0 +1,41 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformStop : MonoBehaviour
+{
+    public Activator activator;
+    public Vector3 startingVelocity;
+    public float movementDuration;
+    public float timer;
+    Rigidbody2D body;
+    private bool activated = false;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (activator.isActive)
+        {
+            activated = true;
+        }
+
+        if (activated)
+        {
+            timer += Time.deltaTime;
+
+            if (timer > movementDuration)
+            {
+                body.velocity = Vector2.zero;
+            }
+            else
+            {
+                body.velocity = startingVelocity;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WilScripts/ScaleAnimationController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    private Activator scaleActivator;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animator = this.GetComponent<Animator>();
+        scaleActivator = this.GetComponentInParent<Activator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (scaleActivator.isActive)
+        {
+            animator.SetBool("activated", true);
+        }
+        else
+        {
+            animator.SetBool("activated", false);
+        }
+    }
+}
